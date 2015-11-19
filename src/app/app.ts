82823aeb3a744c18405004ccaa014882bcdb2643
Mark Hobson <markhobson@gmail.com>
2015-11-19 11:45:59 +0000

    Displayed more hero details

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,12 @@
 import {bootstrap, Component} from 'angular2/angular2';
 @Component({
     selector: 'my-app',
-    template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>'
+    template: `
+        <h1>{{title}}</h1>
+        <h2>{{hero.name}} details!</h2>
+        <div><label>id: </label>{{hero.id}}</div>
+        <div><label>name: </label>{{hero.name}}</div>
+        `
 })
 class AppComponent {
     public title = 'Tour of Heroes';
