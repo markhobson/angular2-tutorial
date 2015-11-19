8fdbbf9461003970a093ccd7a6ba0a237cc2c67e
Mark Hobson <markhobson@gmail.com>
2015-11-19 11:48:49 +0000

    Make hero name editable

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,7 +5,10 @@ import {bootstrap, Component} from 'angular2/angular2';
         <h1>{{title}}</h1>
         <h2>{{hero.name}} details!</h2>
         <div><label>id: </label>{{hero.id}}</div>
-        <div><label>name: </label>{{hero.name}}</div>
+        <div>
+            <label>name: </label>
+            <div><input value="{{hero.name}}" placeholder="name"/></div>
+        </div>
         `
 })
 class AppComponent {
