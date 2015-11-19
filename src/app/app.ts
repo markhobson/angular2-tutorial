30a38a022ee399ed52956e5aa3e3922adc5590c6
Mark Hobson <markhobson@gmail.com>
2015-11-19 11:41:34 +0000

    Introduced Hero class

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,17 @@
 import {bootstrap, Component} from 'angular2/angular2';
 @Component({
     selector: 'my-app',
-    template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>'
+    template: '<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>'
 })
 class AppComponent {
     public title = 'Tour of Heroes';
-    public hero = 'Windstorm';
+    public hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    };
+}
+class Hero {
+    id: number;
+    name: string;
 }
 bootstrap(AppComponent);
